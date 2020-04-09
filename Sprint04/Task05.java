42e87f956b0aa3128195033ee387b0256ee8cd75
Oleksandr Butrym <butrym@karazin.ua>
2020-04-09 15:54:10 +0300

    Create Task05.java

---
+++ Sprint04/Task05.java
@@ -0,0 +1,25 @@
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int[] getXYPair() {
+        return new int[] {x, y};
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((x -= this.x) * x + (y -= this.y) * y);
+    }
+
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+
+    public double distance() {
+        return distance(0, 0);
+    }
+}
