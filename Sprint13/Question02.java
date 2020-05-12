a54d4cf7555cc84ed8ff0deebe03a7c0c6c31455
Oleksandr Butrym <butrym@karazin.ua>
2020-05-12 08:56:42 +0300

    lastDayOfMonth

---
+++ Sprint13/Question02.java
@@ -0,0 +1,26 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/*
+Write a method to get the name of last day of the given month in given year.
+Use GregorianCalendar class to solve this exercise.
+The name of day must corresponds next format: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.
+If month isn't correct then name is "Wrong Month".
+ */
+
+class Question02 {
+    public static String lastDayOfMonth(int month, int year) {
+        if (month < Calendar.JANUARY || Calendar.DECEMBER < month) {
+            return "Wrong Month";
+        }
+        Calendar calendar = new GregorianCalendar(year, month + 1, 1); // next month
+        calendar.add(Calendar.DAY_OF_MONTH, -1); // one day back
+        return calendar.getDisplayName(
+                Calendar.DAY_OF_WEEK, Calendar.LONG, java.util.Locale.US);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lastDayOfMonth(Calendar.DECEMBER, 2020));
+        System.out.println(lastDayOfMonth(Calendar.JANUARY, 2020));
+    }
+}
