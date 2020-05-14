de275917e9afc3f2644cf1ac9a7b3c26fda4fc55
Oleksandr Butrym <butrym@karazin.ua>
2020-05-14 17:36:46 +0300

    duplicateElements

---
+++ Sprint14/Question05.java
@@ -0,0 +1,46 @@
+/*
+Create a Stream<Integer> duplicateElements(Stream<Integer> stream) method of the MyUtils class
+ to return a sorted stream of duplicated elements of the input stream.
+For example, for a given elements
+[3, 2, 1, 1, 12, 3, 8, 2, 4, 2]
+you should get
+[1, 2, 3]
+*/
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class MyUtils {
+    public Stream<Integer> duplicateElements(Stream<Integer> stream) {
+        return stream.filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        TreeMap::new,
+                        Collectors.counting()))
+                .entrySet().stream()
+                .filter(mapEntry -> mapEntry.getValue() > 1)
+                .map(Map.Entry::getKey);
+    }
+    // Alternative:
+/*  public Stream<Integer> duplicateElements(Stream<Integer> stream) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        return stream.filter(Objects::nonNull)
+                .peek(i -> seen.merge(i, 1, Integer::sum))
+                .filter(i -> seen.get(i) == 2)
+                .sorted();
+    }
+*/
+
+    public static void main(String[] args) {
+        var in = Stream.of(3, 2, 1, 1, 12, 3, 8, 2, 4, 2);
+        var expected = new Integer[] {1, 2, 3};
+        var out = new MyUtils().duplicateElements(in);
+//        System.out.println(Arrays.toString(out.toArray()));
+        System.out.println(Arrays.equals(out.toArray(), expected) ? "OK" : "FAIL");
+    }
+}
